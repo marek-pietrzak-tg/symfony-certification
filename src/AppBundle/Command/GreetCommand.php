95645762405339e073ba470dc73d43304ef73113
Marek Pietrzak <mpietrzak@inviqa.com>
2015-05-20 18:54:52 +0100

    Add custom colouring the output

--- src/AppBundle/Command/GreetCommand.php
+++ src/AppBundle/Command/GreetCommand.php
@@ -7,6 +7,7 @@ use \Symfony\Component\Console\Input\InputArgument;
 use \Symfony\Component\Console\Input\InputOption;
 use \Symfony\Component\Console\Input\InputInterface;
 use \Symfony\Component\Console\Output\OutputInterface;
+use \Symfony\Component\Console\Formatter\OutputFormatterStyle;
 
 class GreetCommand extends Command
 {
@@ -27,9 +28,27 @@ class GreetCommand extends Command
         }
 
         $output->writeln($message);
+
+        /*---------------------- tags ----------------------------*/
         $output->writeln('<info>Green text</info>');
         $output->writeln('<comment>Yellow text</comment>');
         $output->writeln('<question>Black text on cyan background</question>');
         $output->writeln('<error>White text on red background</error>');
+
+        /*---------------------- custom styles ----------------------------*/
+        // available colours: black, red, green, yellow, blue, magenta, cyan and white.
+        // available options: bold, underscore, blink, reverse and conceal.
+        $style = new OutputFormatterStyle('red', 'yellow', ['bold']);
+        $output->getFormatter()->setStyle('fire', $style);
+        $output->writeln('<fire>This text is on fire</fire>');
+
+        $style->setOption('reverse');
+        $output->writeln('<fire>Style now reversed</fire>');
+
+        $greenStyle = new OutputFormatterStyle('green', 'white', ['bold', 'underscore']);
+        $output->writeln($greenStyle->apply('this is a different way of applying style to the text'));
+
+        /*---------------------- custom style in tag ----------------------------*/
+        $output->writeln('<fg=magenta;bg=green;options=bold>Custom style in tag</fg=magenta;bg=green;options=bold>');
     }
 }
