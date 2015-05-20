a2f376ba7b36cf615a78779df2f6e5b30b6be810
Marek Pietrzak <mpietrzak@inviqa.com>
2015-05-20 13:39:36 +0100

    Add greet command

---
+++ src/AppBundle/Command/GreetCommand.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace AppBundle\Command;
+
+use \Symfony\Component\Console\Command\Command;
+use \Symfony\Component\Console\Input\InputArgument;
+use \Symfony\Component\Console\Input\InputOption;
+use \Symfony\Component\Console\Input\InputInterface;
+use \Symfony\Component\Console\Output\OutputInterface;
+
+class GreetCommand extends Command
+{
+    protected function configure()
+    {
+        $this->setName('demo:greet')
+            ->setDescription('Greet someone')
+            ->addArgument('name', InputArgument::OPTIONAL, 'Who do you want to greet?', 'world')
+            ->addOption('yell', 'y', InputOption::VALUE_NONE, 'If set then greeting will be uppercase');
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        $message = sprintf("Hello %s", $input->getArgument('name'));
+
+        if ($input->getOption('yell')) {
+            $message = strtoupper($message);
+        }
+
+        $output->writeln($message);
+    }
+}
