06633da5ac5d2357100a1268489211a18af3868b
Marek Pietrzak <mpietrzak@inviqa.com>
2015-06-11 16:37:45 +0100

    Add command executing another one

---
+++ src/AppBundle/Command/MetaCommand.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace AppBundle\Command;
+
+use \Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\ArrayInput;
+use \Symfony\Component\Console\Input\InputArgument;
+use \Symfony\Component\Console\Input\InputOption;
+use \Symfony\Component\Console\Input\InputInterface;
+use \Symfony\Component\Console\Output\OutputInterface;
+use \Symfony\Component\Console\Formatter\OutputFormatterStyle;
+
+class MetaCommand extends Command
+{
+    protected function configure()
+    {
+        $this->setName('demo:meta')
+            ->setDescription('Meta command executing greet one')
+            ->addArgument('name', InputArgument::OPTIONAL, 'Who do you want to greet?', 'world');
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        $greetCommand = $this->getApplication()->find('demo:greet');
+        $parameters = [
+            'name' => $input->getArgument('name'),
+            '--yell' => true
+        ];
+
+        $inputArray = new ArrayInput($parameters);
+
+        $returnCode = $greetCommand->run($inputArray, $output);
+        $output->writeln(sprintf('<info>Greet command finished with code %d</info>', $returnCode));
+    }
+}
